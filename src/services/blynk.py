69ebbdcaa5010fe06220611f59494c5968f20059
Shawn Cahill <shawnjcahill@gmail.com>
2026-05-03 21:06:33 +0100

    Added to blynk file and website to check labels for temp alerts and humidity alerts Running from this for now will move into main wheni am running in full but checking each piece on its own first

--- src/services/blynk.py
+++ src/services/blynk.py
@@ -2,7 +2,11 @@
 from time import time, sleep
 
 import BlynkLib
-from src.config import BLYNK_AUTH_TOKEN
+from src.config import (
+    BLYNK_AUTH_TOKEN,
+    TEMP_HIGH_THRESHOLD,
+    HUMIDITY_HIGH_THRESHOLD
+)
 
 from sense_hat import SenseHat
 
@@ -30,10 +34,21 @@ if __name__ == "__main__":
             temperature = round(sense.get_temperature(), 1)
             humidity = round(sense.get_humidity(), 1)
 
+            temp_alert = 1 if temperature > TEMP_HIGH_THRESHOLD else 0
+            humidity_alert = 1 if humidity > HUMIDITY_HIGH_THRESHOLD else 0
+
             blynk.virtual_write(1, temperature)
             blynk.virtual_write(2, humidity)
+            blynk.virtual_write(3, temp_alert)
+            blynk.virtual_write(4, humidity_alert)
+            
 
-            print(f"Temperature: {temperature} C | Humidity: {humidity} %")
+            print(
+                f"Temperature: {temperature} C | "
+                f"Humidity: {humidity} % | "
+                f"Temp alert: {temp_alert} | "
+                f"Humidity alert: {humidity_alert}"
+            )
 
             sleep(2)
 
